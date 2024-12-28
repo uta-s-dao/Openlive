cb13ecc8c95039cc4a75a3dd11aa92bac1083d5a
uta-s-dao <saruwatari.yuta379@mail.kyutech.jp>
2024-12-28 22:54:14 +0900

    my sql のサーバーを追加する前まで

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import Link from "next/link";
+import Image from "next/image";
+import Live from "../../public/S__23707650.jpg";
 export default function Home() {
   return (
-    <div className=''>
-      <h1 className='text-4xl'>OpenLive</h1>
-      <Link className='text-blue-500 underline text-2xl' href='./todo'>
+    <div>
+      <div className='flex items-end bg-gradient-to-t'>
+        <h1 className='text-5xl'>Openlive</h1>
+        <h1 className='text-xl  ml-auto'>By colllege students</h1>
+      </div>
+      <Image src={Live} alt='Live picture' className='' />
+      <Link
+        className='text-blue-500 underline text-4xl hover:bg-sky-200'
+        href='./todo'
+      >
         todo
       </Link>
     </div>
